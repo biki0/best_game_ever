689f6fed91503d634a4ce8e1bc0187384b9e49f8
biki0 <bikashkhatiwoda0@gmail.com>
2023-05-30 21:34:49 -0400

    clean up

--- src/LevelManager.cpp
+++ src/LevelManager.cpp
@@ -111,55 +111,11 @@ void LevelManager::load_level(){
 }
 
 void LevelManager::render(){
-
-    
     
     for(int i=0; i<m_entities.size(); ++i){
 
         m_entities[i]->render();
 
     }
-
-    // int tile_id = 0;
-
-
-    // for(int x = 0; x < TILE_ROW; ++x){
-    //     for(int y = 0; y < TILE_COL; ++y){        
-
-    //         tile_id = m_level_map[x][y];
-
-    //         SDL_Rect temp_rect;
-    //         temp_rect.w = 32;
-    //         temp_rect.h = 32;
-    //         temp_rect.x = y * 32;
-    //         temp_rect.y = x * 32;
-
-    //         Vector_2d pos;
-    //         pos.x = y*32;
-    //         pos.y = x*32;
-            
-    //         switch(tile_id) {
-                
-    //             case 0: {
-    //                 break;
-    //             }
-    //             case 1: {
-
-    //                 m_texture_rects.push_back(temp_rect);
-    //                 //m_entities.push_back(Entity(m_textures["grass"], pos, 32, 32));
-    //                 TextureManager::RenderTexture(m_renderer, m_textures["grass"], temp_rect);
-                    
-    //                 break;
-    //             }
-    //             default: {
-    //                 break;
-    //             }
-                
-    //         }
-                
-    //     }
-    // }
-
-    //std::cout << idk[10].y<< "XD\n";
 
 }
